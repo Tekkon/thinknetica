6f0dec2ab5365047e9bc0dcb208ddacc8b2eaa5e
Nikita <ningen.tos@gmail.com>
2017-08-11 22:18:03 +0500

    1. В моделях перенес описание ассоциаций перед описанием валидаций 2. В Question добавил опцию dependent: :destroy, чтобы связанные с вопросом ответы удалялись автоматически при удалении вопроса. 3. Исключил database.yml из репозитория 4. Исправил миграции 5. Изменил порядок тестов: сначала ассоциации, потом валидации

--- db/migrate/20170811111522_add_title_and_body_to_question.rb
+++
@@ -1,6 +0,0 @@
-class AddTitleAndBodyToQuestion < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :questions, :title, :string
-  	add_column :questions, :body, :text
-  end
-end
--- db/migrate/20170811142715_delete_title_from_answer.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTitleFromAnswer < ActiveRecord::Migration[5.0]
-  def change
-  	remove_column :answers, :title
-  end
-end
