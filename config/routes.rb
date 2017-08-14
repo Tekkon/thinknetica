ba9ca5b818e6e768944187cf2e01eb05b911eb21
Nikita <ningen.tos@gmail.com>
2017-08-14 17:09:50 +0500

    Пользователь может выйти из системы

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "questions#index"
 
+  devise_scope :user do
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
   resources :questions do
   	resources :answers
   end
---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'User sign out', %q{
+  In order to be able to ask questions
+  As an user
+  I want to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user tries to sign out' do
+    sign_in(user)
+
+    click_on 'Sign out'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+
+  scenario 'Non-authenticated user tries to sign out' do
+    visit root_path
+    expect(page).not_to have_content 'Sign out.'
+  end
+
+end
