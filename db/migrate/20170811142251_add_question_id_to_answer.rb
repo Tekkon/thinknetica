59dc65ddafa94570d777a3a001d724a5f2f0799a
Nikita <ningen.tos@gmail.com>
2017-08-15 10:54:10 +0500

    Добавил автора к вопросу и ответу

--- db/migrate/20170811142251_add_question_id_to_answer.rb
+++ db/migrate/20170811142251_add_question_id_to_answer.rb
@@ -1,5 +1,5 @@
 class AddQuestionIdToAnswer < ActiveRecord::Migration[5.0]
   def change
-  	add_belongs_to :answers, :question_id, :integer
+  	add_belongs_to :answers, :question
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814084643) do
+ActiveRecord::Schema.define(version: 20170815055059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20170814084643) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "question_id"
+    t.integer  "user_id"
+    t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
+    t.index ["user_id"], name: "index_answers_on_user_id", using: :btree
   end
 
   create_table "questions", force: :cascade do |t|
@@ -27,6 +30,8 @@ ActiveRecord::Schema.define(version: 20170814084643) do
     t.datetime "updated_at", null: false
     t.string   "title"
     t.text     "body"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
