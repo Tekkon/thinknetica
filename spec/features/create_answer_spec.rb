f1fdb93e997045983e174a7add979a4aef526dd8
Nikita <ningen.tos@gmail.com>
2017-08-17 23:18:21 +0500

    Применил append для добавления нового вопроса

--- spec/features/create_answer_spec.rb
+++ spec/features/create_answer_spec.rb
@@ -17,7 +17,7 @@ feature 'User can create an answer on the question page', %q{
     click_on 'Create an answer'
 
     expect(current_path).to eq question_path(question)
-    within '.answer' do
+    within '.answers' do
       expect(page).to have_content 'This is my answer.'
     end
   end
