abed6cbddfafb1b9518b08b936e141dfaaf70e12
Nikita <ningen.tos@gmail.com>
2017-08-24 22:52:43 +0500

    Добавил скоуп для сортировки ответов (лучший ответ первый)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,8 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true
 
+  scope :ordered_by_favorite, -> { order(favorite: :desc) }
+
   def mark_favorite
     self.update(favorite: true)
     self.question.unmark_favorites(self)
