05e0dadfd4ba9b876f32f2dd2114c44b295178ae
Nikita <ningen.tos@gmail.com>
2017-08-25 20:49:50 +0500

    Плиморфные ассоциации между answer, question и attachment

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,11 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+  has_many :attachments, as: :attachmentable, dependent: :destroy
 
   validates :body, presence: true
+
+  accepts_nested_attributes_for :attachments
 
   scope :ordered_by_favorite, -> { order(favorite: :desc) }
 
