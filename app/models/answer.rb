d58f8de9330dfe2d84a138cd8142b1b89a509ae1
Nikita <ningen.tos@gmail.com>
2017-08-29 09:50:54 +0500

    Вынес все, что связано с вложениями в косерн Attachmentable

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,11 +1,10 @@
 class Answer < ApplicationRecord
+  include Attachmentable
+
   belongs_to :question
   belongs_to :user
-  has_many :attachments, as: :attachmentable, dependent: :destroy
 
   validates :body, presence: true
-
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank
 
   scope :ordered_by_favorite, -> { order(favorite: :desc) }
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,8 @@
 class Question < ApplicationRecord
+  include Attachmentable
+
   has_many :answers, dependent: :destroy
-  has_many :attachments, as: :attachmentable, dependent: :destroy
   belongs_to :user
 
   validates :title, :body, presence: true
-
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank
 end
