d26d7518acb342f5be5f40e231a4ab50788357b5
Nikita <ningen.tos@gmail.com>
2017-09-05 23:36:43 +0500

    Добавил тест на метод #filename

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Attachment, type: :model do
+  let(:user) { create(:user) }
+  let!(:question) { create(:question, user: user) }
+  let!(:attachment) { create(:attachment, attachmentable: question, file: Rails.root.join("spec/spec_helper.rb").open)}
+
   it { should belong_to :attachmentable }
+
+  it "#filename" do
+    expect(attachment.filename).to eq 'spec_helper.rb'
+  end
 end
