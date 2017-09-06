61ab3de982a87c5a8a4a3ba46012c5dcdba8958c
Nikita <ningen.tos@gmail.com>
2017-09-06 09:22:36 +0500

    Модель Comment и консерн Commentable

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   include Attachmentable
   include Votable
+  include Commentable
 
   has_many :answers, dependent: :destroy
   belongs_to :user
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 require_relative 'concerns/votable_spec.rb'
 require_relative 'concerns/attachmentable_spec.rb'
+require_relative 'concerns/commentable_spec.rb'
 
 RSpec.describe Answer, type: :model do
   it_behaves_like 'votable'
   it_behaves_like 'attachmentable'
+  it_behaves_like 'commentable'
 
   it { should belong_to(:question) }
   it { should belong_to(:user) }
