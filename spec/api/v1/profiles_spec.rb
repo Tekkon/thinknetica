e3925de722d138982d42b9f288d0e886a45dae15
Nikita <ningen.tos@gmail.com>
2017-09-12 19:46:33 +0500

    API для cписка профилей всех пользователей, за исключением аутентифицированного

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -37,4 +37,66 @@ describe 'Profile API' do
       end
     end
   end
+
+  describe 'GET /all' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/profiles/all', format: :json
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get '/api/v1/profiles/all', format: :json, access_token: '1234'
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let!(:me) { create(:user) }
+      let!(:users) { create_list(:user, 2) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get '/api/v1/profiles/all', format: :json, access_token: access_token.token  }
+
+      it 'returns status 200' do
+        expect(response).to be_success
+      end
+
+      it 'renders list of users' do
+        items = parse_json(response.body)
+        expect(items[0]['id']).to eq users[0].id
+        expect(items[1]['id']).to eq users[1].id
+      end
+
+      it 'does not render authenticated user' do
+        parse_json(response.body).each do |item|
+          expect(item['id']).to_not eq me.id
+        end
+      end
+
+      %w(id email admin).each do |attr|
+        it "contains #{attr}" do
+          items = parse_json(response.body)
+          expect(items[0]["#{attr}"]).to eq users[0].send(attr.to_sym)
+          expect(items[1]["#{attr}"]).to eq users[1].send(attr.to_sym)
+        end
+      end
+
+      %w(created_at updated_at).each do |attr|
+        it "contains #{attr}" do
+          items = parse_json(response.body)
+          expect(items[0]["#{attr}"].to_json).to eq users[0].send(attr.to_sym).to_json
+          expect(items[1]["#{attr}"].to_json).to eq users[1].send(attr.to_sym).to_json
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "does not contain #{attr}" do
+          parse_json(response.body).each do |item|
+            expect(item["#{attr}"]).to eq nil
+          end
+        end
+      end
+    end
+  end
 end
