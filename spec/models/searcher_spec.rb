c176dd7daf4014d4870d6c1cce58518709bf6f12
Nikita <ningen.tos@gmail.com>
2017-09-25 20:07:39 +0500

    refactor

--- spec/models/searcher_spec.rb
+++ spec/models/searcher_spec.rb
@@ -15,27 +15,35 @@ RSpec.describe Searcher, type: :model do
           expect(klass.classify.constantize).to receive(:search).with('new', page: 1)
           Searcher.search(object, 'new')
         end
-
-        it 'finds object' do
-          if object == 'all'
-            klass.classify.constantize.stub(:search).and_return([user, question, answer, comment])
-          else
-            klass.classify.constantize.stub(:search).and_return([eval(object)])
-          end
-
-          result = Searcher.search(object, 'new')
-
-          if object == 'user' || object == 'all'
-            expect(result).to include({ id: user.id, type: 'User', title: user.email, body: user.email })
-          elsif object == 'question' || object == 'all'
-            expect(result).to include({ id: question.id, type: 'Question', title: question.title, body: question.body })
-          elsif object == 'answer' || object == 'all'
-            expect(result).to include({ id: question.id, type: 'Answer', title: answer.body, body: answer.body })
-          elsif object == 'comment' || object == 'all'
-            expect(result).to include({ id: question.id, type: 'Comment', title: comment.body, body: comment.body })
-          end
-        end
       end
+    end
+
+    it 'finds question' do
+      Question.stub(:search).and_return([question])
+      expect(Searcher.search('question', 'new')).to include({ id: question.id, type: 'Question', title: question.title, body: question.body })
+    end
+
+    it 'finds answer' do
+      Answer.stub(:search).and_return([answer])
+      expect(Searcher.search('answer', 'new')).to include({ id: question.id, type: 'Answer', title: answer.body, body: answer.body })
+    end
+
+    it 'finds comment' do
+      Comment.stub(:search).and_return([comment])
+      expect(Searcher.search('comment', 'new')).to include({ id: question.id, type: 'Comment', title: comment.body, body: comment.body })
+    end
+
+    it 'finds user' do
+      User.stub(:search).and_return([user])
+      expect(Searcher.search('user', 'new')).to include({ id: user.id, type: 'User', title: user.email, body: user.email })
+    end
+
+    it 'finds all objects' do
+      ThinkingSphinx.stub(:search).and_return([user, question, answer, comment])
+      expect(Searcher.search('all', 'new')).to include({ id: question.id, type: 'Question', title: question.title, body: question.body })
+      expect(Searcher.search('all', 'new')).to include({ id: question.id, type: 'Answer', title: answer.body, body: answer.body })
+      expect(Searcher.search('all', 'new')).to include({ id: question.id, type: 'Comment', title: comment.body, body: comment.body })
+      expect(Searcher.search('all', 'new')).to include({ id: user.id, type: 'User', title: user.email, body: user.email })
     end
   end
 end
